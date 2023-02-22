508e3c7c584d33cd864054af93318c61a1478868
Yury Borets <udborets@gmail.com>
2023-02-22 21:31:40 +0300

    clear init styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styles/index.css'
 
+console.log('chlen')
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
