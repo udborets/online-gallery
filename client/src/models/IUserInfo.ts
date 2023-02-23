9b195befe5e31f183f944a4edf7f87190f96a1db
Yury Borets <udborets@gmail.com>
2023-02-23 15:00:24 +0300

    create user info interface

---
+++ client/src/models/IUserInfo.ts
@@ -0,0 +1,11 @@
+export default interface IUserInfo {
+  id?: string | null;
+  name?: string | null;
+  email?: string | null;
+  avatar?: string | null;
+  role?: string | null;
+  albums?: any | null;
+  photos?: any | null;
+  createdAt?: any | null;
+  updatedAt?: any | null;
+}
--- client/src/models/IUserSlice.ts
+++ client/src/models/IUserSlice.ts
@@ -1,15 +1,7 @@
+import IUserInfo from "./IUserInfo";
+
 export default interface IUserSlice {
-  userInfo: {
-    id: string | null;
-    name: string | null;
-    email: string | null;
-    avatar: string | null;
-    role: string | null;
-    albums: any | null;
-    photos: any | null;
-    createdAt: any | null;
-    updatedAt: any | null;
-  };
-  isAuth: boolean;
-  token: string | null;
+  userInfo: IUserInfo;
+  isAuth?: boolean;
+  token?: string | null;
 }
