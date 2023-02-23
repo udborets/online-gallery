2de6a3f50e42e38d9d4f38bc655d4a7f99b170b2
Yury Borets <udborets@gmail.com>
2023-02-23 10:08:33 +0300

    add redux provider to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { store } from './store'
 import './styles/index.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
