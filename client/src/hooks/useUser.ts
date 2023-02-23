ccbe328832db4a22255e27aca6312c1755919ab3
Yury Borets <udborets@gmail.com>
2023-02-23 21:40:47 +0300

    add user isAuth const to export

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -1,10 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import { deleteUserState, updateUserState } from "../store/slices/userSlice";
 import IStore from "./../models/IStore";
-import IUserSlice from './../models/IUserSlice';
+import IUserSlice from "./../models/IUserSlice";
 
 export default function useUser() {
   const user = useSelector((store: IStore) => store.user);
+  const isAuth = user.isAuth;
   const dispatch = useDispatch();
   function deleteUser() {
     dispatch(deleteUserState());
@@ -12,5 +13,5 @@ export default function useUser() {
   function updateUser(userInfo: IUserSlice) {
     dispatch(updateUserState(userInfo));
   }
-  return { user, deleteUser, updateUser };
+  return { user, deleteUser, updateUser, isAuth };
 }
