4aa1193315e529c674a5be6167656e8e3f17f659
Yury Borets <udborets@gmail.com>
2023-02-23 15:00:42 +0300

    create redux user hook

---
+++ client/src/hooks/useUser.ts
@@ -0,0 +1,16 @@
+import { useSelector, useDispatch } from "react-redux";
+import { deleteUserState, updateUserState } from "../store/slices/userSlice";
+import IStore from "./../models/IStore";
+import IUserSlice from './../models/IUserSlice';
+
+export default function useUser() {
+  const user = useSelector((store: IStore) => store.user);
+  const dispatch = useDispatch();
+  function deleteUser() {
+    dispatch(deleteUserState());
+  }
+  function updateUser(userInfo: IUserSlice) {
+    dispatch(updateUserState(userInfo));
+  }
+  return { user, deleteUser, updateUser };
+}
