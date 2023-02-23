a42346737aaa817d5c939866d36f978aaeb7d31f
Yury Borets <udborets@gmail.com>
2023-02-23 15:01:34 +0300

    delete auth page functionality

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,17 +1,8 @@
-import { useLocation, useNavigate } from "react-router-dom"
-import { RoutePaths } from "../utils/consts";
+import "../styles/pages/AuthPage.scss";
 
 export default function AuthPage() {
-  const path = useLocation().pathname;
-  const isLogin = path === RoutePaths.LOGIN;
-  const navigate = useNavigate()
   return (
-    <div>
-      <input type="text" />
-      <input type="text" />
-      <button onClick={() => navigate(isLogin ? RoutePaths.REGISTRATION : RoutePaths.LOGIN)}>
-        {isLogin ? "Registration" : "Login"}
-      </button>
+    <div className="auth-page">
     </div>
   )
 }
