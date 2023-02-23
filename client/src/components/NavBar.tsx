c9b59276d3edb6401fca12a7103f80924f9b871d
Yury Borets <udborets@gmail.com>
2023-02-23 22:09:24 +0300

    add custom hook usage in navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,55 +2,20 @@ import { NavLink } from "react-router-dom"
 import { RoutePaths } from "../utils/consts"
 import "../styles/components/NavBar.scss";
 import useUser from './../hooks/useUser';
-import { getAuth, getRedirectResult, signInWithRedirect, signOut } from "firebase/auth";
-import provider from './../auth/provider';
-import useServer from "../hooks/useServer";
+import { getAuth, signInWithRedirect, signOut } from "firebase/auth";
+import provider from '../firebase/provider';
 import { useEffect } from 'react';
+import useLogin from './../hooks/useLogin';
 
 
 const NavBar = () => {
-  const { isAuth, updateUser, deleteUser, user } = useUser();
-  const { getUserByEmail, createUser } = useServer();
+  const { isAuth, deleteUser, user } = useUser();
+  const { loginUser } = useLogin();
   const auth = getAuth();
   useEffect(() => {
-    getRedirectResult(auth)
-      .then(async (result) => {
-        console.log('helloing', result);
-        const currentUser = auth.currentUser;
-        if (!currentUser) {
-          return;
-        }
-        if (currentUser.email) {
-          const isDbHasUser = await getUserByEmail(currentUser.email);
-          if (isDbHasUser) {
-            console.log('db has user, update state')
-            updateUser({
-              userInfo: {
-                ...isDbHasUser,
-              },
-              token: await currentUser.getIdToken(),
-              isAuth: true,
-            })
-          }
-          if (!isDbHasUser) {
-            console.log('db dont have, update state')
-            await createUser(currentUser.email, currentUser.displayName ?? "unknown")
-            updateUser({
-              userInfo: {
-                email: currentUser.email,
-                name: currentUser.displayName ?? "unknown",
-              },
-              token: await auth.currentUser.getIdToken(),
-              isAuth: true,
-            })
-          }
-        }
-      }).catch((error) => {
-        console.log(error)
-      });
+    loginUser();
   }, [])
   async function userSignIn() {
-    console.log('hellong')
     signInWithRedirect(auth, provider);
 
   }
