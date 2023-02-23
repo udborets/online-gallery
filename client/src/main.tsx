c413b28ad1b92653124c89b932a29333e43c3e0b
Yury Borets <udborets@gmail.com>
2023-02-23 15:00:02 +0300

    add firebase to main app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import { store } from './store'
-import './styles/index.scss'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { store } from './store';
+import './styles/index.scss';
+import './auth/firebase';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
