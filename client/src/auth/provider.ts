943c5a4afff76ecc458735908c66f70b936a1277
Yury Borets <udborets@gmail.com>
2023-02-23 21:37:19 +0300

    add provider param to ask email every "Login"

--- client/src/auth/provider.ts
+++ client/src/auth/provider.ts
@@ -2,4 +2,6 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 
+provider.setCustomParameters({prompt: "select_account"})
+
 export default provider;
