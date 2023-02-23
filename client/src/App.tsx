94dcc6adedd7b8eff22cc5f62be921ec06ea9eb3
Yury Borets <udborets@gmail.com>
2023-02-23 15:01:50 +0300

    add navbar to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import AuthPage from './pages/AuthPage'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AuthPage />} />
-      <Route path="/auth" >
-        <Route path="/auth/login" element={<AuthPage />}/>
-        <Route path="/auth/registration" element={<AuthPage />}/>
-      </Route>
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" >
+          <Route path="/auth/login" element={<AuthPage />} />
+          <Route path="/auth/registration" element={<AuthPage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
