48a798f38c89369297f89300e66eeef5a5abe5d0
Yury Borets <udborets@gmail.com>
2023-02-23 15:01:10 +0300

    delete use auth hook

--- client/src/hooks/useAuth.ts
+++
@@ -1,7 +0,0 @@
-import { useSelector } from "react-redux";
-import IStore from "./../models/IStore";
-
-export default function useAuth() {
-  const user = useSelector((store: IStore) => store.user);
-  return user.isAuth;
-}
