d45c1cd2b7b82277ab1b738bd083c1bfc1d7c93c
Yury Borets <udborets@gmail.com>
2023-02-24 23:43:10 +0300

    add "create album" function

--- client/src/hooks/useServer.ts
+++ client/src/hooks/useServer.ts
@@ -4,9 +4,11 @@ export default function useServer() {
   async function getUserById(userId: string) {
     return await client.users.getUserById.query({ userId: userId });
   }
+
   async function getUserByEmail(userEmail: string) {
     return await client.users.getUserByEmail.query({ userEmail: userEmail });
   }
+
   async function createUser(userEmail: string, userName: string) {
     return await client.users.createUser.query({
       userEmail: userEmail,
@@ -22,11 +24,26 @@ export default function useServer() {
     return await client.users.getAll.query();
   }
 
+  async function createAlbum(
+    userId: string,
+    albumName: string,
+    albumDescription: string,
+    isPrivate?: boolean
+  ) {
+    await client.users.addAlbum.query({
+      userId: userId,
+      albumName: albumName,
+      albumDescription: albumDescription,
+      isPrivate: isPrivate ?? false,
+    });
+  }
+
   return {
     getUserById,
     createUser,
     deleteAllUsers,
     getUserByEmail,
     getAllUsers,
+    createAlbum,
   };
 }
