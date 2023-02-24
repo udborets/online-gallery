346918aae5c96b786357c19528149318381b91b8
Yury Borets <udborets@gmail.com>
2023-02-24 11:51:57 +0300

    add login function on page enter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react';
 import NavBar from './components/NavBar'
-import AuthPage from './pages/AuthPage'
-import Home from './pages/Home'
+import useLogin from './hooks/useLogin';
+import PageRouter from './pages/PageRouter';
 
 export default function App() {
+  const { checkAndLogin } = useLogin();
+  useEffect(() => {
+    checkAndLogin();
+  }, [])
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" >
-          <Route path="/auth/login" element={<AuthPage />} />
-          <Route path="/auth/registration" element={<AuthPage />} />
-        </Route>
-      </Routes>
+      <PageRouter />
     </>
   )
 }
