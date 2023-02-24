19cc2e5074bfcb669ccd72eed7504f19f23267b4
Yury Borets <udborets@gmail.com>
2023-02-24 15:01:35 +0300

    add dropdown item component

---
+++ client/src/components/UI/DropDownItem.tsx
@@ -0,0 +1,13 @@
+import IDropDownItemProps from './../../models/IDropDownItemProps';
+
+
+const DropDownItem = ({ text, fn, className }: IDropDownItemProps) => {
+  return (
+    <button onClick={() => fn()}
+      className={className ? className : "nav-profile__option"}>
+      {text}
+    </button>
+  )
+}
+
+export default DropDownItem
