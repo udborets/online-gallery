89d6cbc02b6fa9f51c82c8f77533ede2dcfa38ba
Yury Borets <udborets@gmail.com>
2023-02-24 18:05:21 +0300

    add users information on "/users" page

--- client/src/pages/UsersPage.tsx
+++ client/src/pages/UsersPage.tsx
@@ -1,25 +1,51 @@
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import useServer from "../hooks/useServer";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import IUserInfo from './../models/IUserInfo';
+import { RoutePaths } from "../utils/consts";
+import "../styles/pages/UsersPage.scss";
 
 const UsersPage = () => {
   const { id } = useParams();
-  const { getUserById } = useServer();
-  const [user, setUser] = useState<any>(null);
+  const { getUserById, getAllUsers } = useServer();
   if (id) {
-    getUserById(id).then((userData) => setUser(userData));
+    const [pageUser, setUser] = useState<any>(null);
+    
+    useEffect(() => {
+      getUserById(id).then((userData) => {setUser(userData); console.log('id')});
+    }, [id])
+    return (
+      <div className="users-page">
+        {pageUser ? pageUser.name : "no name"}
+      </div>
+    )
   }
-  return (
-    id
-      ?
-      <div>
-        {user ? user.name : "no name"}
+  else {
+    const [users, setUsers] = useState<any>(null);
+    useEffect(() => {
+      getAllUsers().then((fetchedUsers) => setUsers(fetchedUsers));
+    }, [id])
+    return (
+      <div className="users-page">
+        {Array.isArray(users) ? users.map((user: IUserInfo) => (
+          <div key={user.id}>
+            id: {user.id} <br />
+            name: {user.name} <br />
+            email: {user.email} <br />
+            go to {user.name} user: <NavLink to={RoutePaths.USERS + "/" + user.id}>{RoutePaths.USERS + "/" + user.id}</NavLink><br />
+            go to {user.name} gallery: <NavLink to={RoutePaths.USERS + "/" + user.id + RoutePaths.GALLERY}>
+              {RoutePaths.USERS + "/" + user.id + RoutePaths.GALLERY}
+            </NavLink> <br />
+            <br />
+          </div>
+        )) : "no name"}
+        <button onClick={() => console.log(users)} style={{ background: "black", color: "white" }}>
+          show users from db info
+        </button>
       </div>
-      :
-      <div>
-        users
-      </div>
-  )
+    )
+  }
+
 }
 
 export default UsersPage
