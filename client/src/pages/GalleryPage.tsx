a6fedcfb486eddd79d2534854a5e9febda79f4f9
Yury Borets <udborets@gmail.com>
2023-02-25 22:45:19 +0300

    add user photos fetching on user gallery page

--- client/src/pages/GalleryPage.tsx
+++ client/src/pages/GalleryPage.tsx
@@ -1,28 +1,38 @@
 import useServer from './../hooks/useServer';
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "../styles/pages/GalleryPage.scss";
 import AlbumForm from './../components/AlbumForm';
+import useUser from '../hooks/useUser';
 
 const GalleryPage = () => {
-  const { id } = useParams();
-  const { getUserById } = useServer();
+  const { getUserById, getAllPhotosByAlbumId } = useServer();
   const [currentUser, setCurrentUser] = useState<any>(null);
-  if (id) {
-    getUserById(id).then((fetchedUser: any) => setCurrentUser(fetchedUser))
-    return (
-      <div className='gallery-page'>
-        <AlbumForm {...currentUser} />
-      </div>
-    )
-  }
-  else {
-    return (
-      <div className='gallery-page'>
-        helloing
-      </div>
-    )
-  }
+  const [photos, setPhotos] = useState<any>(null);
+  const { user } = useUser();
+  useEffect(() => {
+    setCurrentUser(user.userInfo);
+    if (currentUser && currentUser.albums) {
+      getAllPhotosByAlbumId(currentUser.albums[0].id).then((fetchedAlbum) => {
+        if (fetchedAlbum) {
+          setPhotos(fetchedAlbum);
+        }
+      });
+    }
+  }, [currentUser])
+  return (
+    <div className='gallery-page'>
+      {currentUser
+        &&
+        <>
+          <AlbumForm {...currentUser} />
+          {photos && photos.map((photo: any) => {
+            return <img src={`${import.meta.env.VITE_REACT_APP_API_URL}/${photo.file}`} key={photo.id} />
+          })}
+        </>
+      }
+    </div>
+  )
 }
 
 export default GalleryPage
