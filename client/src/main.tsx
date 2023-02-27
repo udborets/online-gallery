595f20a640ebccda79fa9bebb5904e8d097dd0b5
Yury Borets <udborets@gmail.com>
2023-02-27 16:45:03 +0300

    add react query client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,13 +6,18 @@ import App from './App';
 import { store } from './store';
 import './styles/index.scss';
 import './firebase/firebase';
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
