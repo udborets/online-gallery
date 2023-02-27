ba11a98f3631b1216f640b3cb21a7cbfeb1dc070
Yury Borets <udborets@gmail.com>
2023-02-27 18:54:22 +0300

    delete unused import

--- client/src/pages/UsersPage.tsx
+++ client/src/pages/UsersPage.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useParams } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import useServer from "../hooks/useServer";
 import { useEffect, useState } from 'react';
 import IUserInfo from './../models/IUserInfo';
