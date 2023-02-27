3fb066231c4550e3866733be72e9044269f77375
Yury Borets <udborets@gmail.com>
2023-02-27 21:05:13 +0300

    add modal and notification reducers to redux store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import modalSlice from "./slices/modalSlice";
+import notificationSlice from "./slices/notificationSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    modal: modalSlice,
+    notification: notificationSlice,
   },
 });
