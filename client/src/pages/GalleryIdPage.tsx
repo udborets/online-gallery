7599ff22cec9fd731dbd6e12c96640504c8dfeec
Yury Borets <udborets@gmail.com>
2023-02-28 23:49:57 +0300

    add photo modal window in gallery by id page

--- client/src/pages/GalleryIdPage.tsx
+++ client/src/pages/GalleryIdPage.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { useQuery } from "react-query/react";
 import { useParams } from "react-router-dom";
+import PhotoFormModal from "../components/modals/PhotoFormModal";
+import ModalTemplate from "../components/templates/ModalTemplate";
 import useServer from "../hooks/useServer";
 
 const GalleryIdPage = () => {
   const { user_id, album_id } = useParams();
+  const [isPhotoModalActive, setIsPhotoModalActive] = useState(false);
   if (!album_id || !user_id) {
     return <div>there is no album_id or user_id parameter in query</div>
   }
@@ -47,12 +51,19 @@ const GalleryIdPage = () => {
   if (isPhotosLoading || isHasAlbumLoading) {
     return <div>Photos loading...</div>
   }
-  
+
   return (
     <div>
       {photos && photos.map((photo: any) => {
         return <img src={`${import.meta.env.VITE_REACT_APP_API_URL}/${photo.file}`} key={photo.id} />
       })}
+
+      <button onClick={() => setIsPhotoModalActive(true)}>
+        photo
+      </button>
+      <ModalTemplate visible={isPhotoModalActive} setVisible={setIsPhotoModalActive} >
+        <PhotoFormModal albumId={album_id} />
+      </ModalTemplate>
     </div>
   )
 }
