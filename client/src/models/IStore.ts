9c60e51558cb36ecdb71d2e27c149e2ea3adfd3d
Yury Borets <udborets@gmail.com>
2023-02-28 23:49:40 +0300

    fix store state interface

--- client/src/models/IStore.ts
+++ client/src/models/IStore.ts
@@ -3,8 +3,9 @@ import { IUserSlice } from "./IUserSlice";
 export interface IStore {
   user: IUserSlice;
   modal: {
-    isActive: boolean;
-    setIsActive: (isActive: boolean) => any;
+    isPhotoActive: boolean;
+    isAlbumActive: boolean;
+    setIsActive: (isPhotoActive: boolean, isAlbumActive: boolean) => any;
   };
   notification: {
     isActive: boolean;
