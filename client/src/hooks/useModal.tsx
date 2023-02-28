51e441f82adde59bacf3efe7cab2157bbca1fc06
Yury Borets <udborets@gmail.com>
2023-02-28 23:49:23 +0300

    add useModal custom hook

--- client/src/hooks/useModal.tsx
+++ client/src/hooks/useModal.tsx
@@ -1,13 +1,13 @@
 import { useSelector } from "react-redux";
-import { IStore } from "./../models/IStore";
+import { IStore } from "../models/IStore";
 import { useDispatch } from "react-redux";
 import { setIsActive } from "../store/slices/modalSlice";
 
 export default function useModal() {
   const modal = useSelector((store: IStore) => store.modal);
   const dispatch = useDispatch();
-  function setModalIsActive(isActive: boolean) {
-    dispatch(setIsActive({ isActive: isActive }));
+  function setModalIsActive(isPhotoActive: boolean, isAlbumActive: boolean) {
+    dispatch(setIsActive({ isPhotoActive: isPhotoActive, isAlbumActive: isAlbumActive, }));
   }
   return { modal, setModalIsActive };
 }
