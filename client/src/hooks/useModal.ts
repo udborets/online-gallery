67a5df299d7987c0b6220e1b1b8b5281a3fa3bc8
Yury Borets <udborets@gmail.com>
2023-02-28 08:00:00 +0300

    add "isActive" control functions in custom hooks

--- client/src/hooks/useModal.ts
+++ client/src/hooks/useModal.ts
@@ -1,13 +1,13 @@
 import { useSelector } from "react-redux";
 import { IStore } from "./../models/IStore";
 import { useDispatch } from "react-redux";
-import { setModalState } from "../store/slices/modalSlice";
+import { setIsActive } from "../store/slices/modalSlice";
 
 export default function useModal() {
-  const modal = useSelector((store: IStore) => store.modal.modalElement);
+  const modal = useSelector((store: IStore) => store.modal);
   const dispatch = useDispatch();
-  function setModal(newModal: JSX.Element) {
-    dispatch(setModalState({ modalElement: newModal }));
+  function setModalIsActive(isActive: boolean) {
+    dispatch(setIsActive({ isActive: isActive }));
   }
-  return { modal, setModal };
+  return { modal, setModalIsActive };
 }
--- client/src/hooks/useNotification.ts
+++ client/src/hooks/useNotification.ts
@@ -1,15 +1,13 @@
 import { useSelector } from "react-redux";
 import { IStore } from "./../models/IStore";
 import { useDispatch } from "react-redux";
-import { setNotificationState } from "../store/slices/notificationSlice";
+import { setIsActive } from "../store/slices/notificationSlice";
 
 export default function useNotification() {
-  const notification = useSelector(
-    (store: IStore) => store.notification.notificationElement
-  );
+  const notification = useSelector((store: IStore) => store.notification);
   const dispatch = useDispatch();
-  function setNotification(newNotification: JSX.Element) {
-    dispatch(setNotificationState({ notificationElement: newNotification }));
+  function setNotificationIsActive(isActive: boolean) {
+    dispatch(setIsActive({ isActive: isActive }));
   }
-  return { notification, setNotification };
+  return { notification, setNotificationIsActive };
 }
