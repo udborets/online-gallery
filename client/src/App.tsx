00177a8842a2219cb8fff80f4486d3c31b29687b
Yury Borets <udborets@gmail.com>
2023-02-28 23:46:02 +0300

    delete modal window from app root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,6 @@ import { useEffect } from 'react';
 import NavBar from './components/NavBar'
 import useLogin from './hooks/useLogin';
 import PageRouter from './PageRouter';
-import ModalTemplate from './components/templates/ModalTemplate';
-import PhotoFormModal from './components/modals/PhotoFormModal';
 
 export default function App() {
   const { checkAndLogin } = useLogin();
@@ -12,9 +10,6 @@ export default function App() {
   }, [])
   return (
     <>
-      <ModalTemplate>
-        <PhotoFormModal />
-      </ModalTemplate>
       <NavBar />
       <PageRouter />
     </>
