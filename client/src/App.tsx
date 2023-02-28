52ece8be2a7f51578ff96dc5683c2b03d789e669
Yury Borets <udborets@gmail.com>
2023-02-28 08:46:25 +0300

    add photo form modal to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import NavBar from './components/NavBar'
 import useLogin from './hooks/useLogin';
 import PageRouter from './PageRouter';
+import ModalTemplate from './components/templates/ModalTemplate';
+import PhotoFormModal from './components/modals/PhotoFormModal';
 
 export default function App() {
   const { checkAndLogin } = useLogin();
@@ -10,6 +12,9 @@ export default function App() {
   }, [])
   return (
     <>
+      <ModalTemplate>
+        <PhotoFormModal />
+      </ModalTemplate>
       <NavBar />
       <PageRouter />
     </>
