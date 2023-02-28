c781af55f8c3491bf0077ded731e8ef5f778134f
Yury Borets <udborets@gmail.com>
2023-02-28 09:45:11 +0300

    fix photo form modal file input

--- client/src/components/modals/PhotoFormModal.tsx
+++ client/src/components/modals/PhotoFormModal.tsx
@@ -5,31 +5,23 @@ import useUser from '../../hooks/useUser';
 const PhotoFormModal = () => {
   const { user, fetchUser } = useUser();
   const [file, setFile] = useState<any>(null);
-  const [isUploaded, setIsUploaded] = useState(false);
   const [customName, setCustomName] = useState('');
   const [photoDescription, setPhotoDescription] = useState('');
-  const [notification, setNotification] = useState('');
   async function uploadFile(e: any) {
     e.preventDefault();
     const formData = new FormData();
     if (!file) {
-      setNotification("You have to set file");
       setTimeout(() => {
-        setNotification('');
       }, 5000);
       return
     }
     if (!customName) {
-      setNotification("Enter photo name");
       setTimeout(() => {
-        setNotification('');
       }, 5000);
       return;
     }
     if (!user.userInfo.id) {
-      setNotification("You have to be authorized");
       setTimeout(() => {
-        setNotification('');
       }, 5000);
       return
     }
@@ -38,13 +30,12 @@ const PhotoFormModal = () => {
     formData.append("albumId", user.userInfo.albums[0].id);
     formData.append("customName", customName);
     formData.append("photoDescription", photoDescription);
-    console.log(file)
     const response = await axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/upload`, formData)
     if (response.data === "OK") {
-      setIsUploaded(true);
       setFile(null);
+      setCustomName('');
+      setPhotoDescription('');
       setTimeout(() => {
-        setIsUploaded(false);
       }, 5000);
       await fetchUser(user.userInfo.id);
     }
@@ -56,13 +47,11 @@ const PhotoFormModal = () => {
     <form className='photo-form' onClick={(e) => e.stopPropagation()}>
       <input
         className='modal-form__name'
-        style={{color: "black"}}
         type="text"
         onChange={(e) => setCustomName(e.target.value)}
         value={customName}
       />
       <textarea
-        style={{color: "black"}}
         className='modal-form__description'
         onChange={(e) => setPhotoDescription(e.target.value)}
         value={photoDescription}
@@ -70,7 +59,6 @@ const PhotoFormModal = () => {
       <input
         type="file"
         name='userFile'
-        value={file}
         onChange={e => setFile(e.target.files ? e.target.files[0] : null)} />
       <button
         className='photo-form__submit'
