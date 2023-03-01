10f730e6d1dbd6e70dd918be7dc0011171ab8b81
Yury Borets <udborets@gmail.com>
2023-03-01 10:27:09 +0300

    fix redux store interface

--- client/src/models/IStore.ts
+++ client/src/models/IStore.ts
@@ -2,11 +2,6 @@ import { IUserSlice } from "./IUserSlice";
 
 export interface IStore {
   user: IUserSlice;
-  modal: {
-    isPhotoActive: boolean;
-    isAlbumActive: boolean;
-    setIsActive: (isPhotoActive: boolean, isAlbumActive: boolean) => any;
-  };
   notification: {
     isActive: boolean;
     setIsActive: (isActive: boolean) => any;
