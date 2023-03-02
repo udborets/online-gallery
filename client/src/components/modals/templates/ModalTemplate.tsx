2e593e327a8d41ae9c1ec8700a442c45fefac4f2
Yury Borets <udborets@gmail.com>
2023-03-02 19:41:14 +0300

    add modal template interface usage

--- client/src/components/modals/templates/ModalTemplate.tsx
+++ client/src/components/modals/templates/ModalTemplate.tsx
@@ -1,6 +1,7 @@
+import { IModalTemplateProps } from "../../../models/IModalsProps"
 import "../../../styles/components/templates/ModalTemplate.scss"
 
-const ModalTemplate = ({ children, visible, setVisible }: any) => {
+const ModalTemplate = ({ children, visible, setVisible }: IModalTemplateProps) => {
   return (
     <div
       className={`modal ${visible ? "visible" : "hidden"}`}
