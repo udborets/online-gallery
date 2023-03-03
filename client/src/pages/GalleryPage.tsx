df18d8dc9a469b4253e7c1f03eebbf18f1701895
Yury Borets <udborets@gmail.com>
2023-03-03 20:56:24 +0300

    update gallery page displaying

--- client/src/pages/GalleryPage.tsx
+++ client/src/pages/GalleryPage.tsx
@@ -4,26 +4,31 @@ import useUser from '../hooks/useUser';
 import ModalTemplate from '../components/modals/templates/ModalTemplate';
 import AlbumFormModal from '../components/modals/AlbumFormModal';
 import AlbumItem from '../components/AlbumItem';
+import useNotification from '../hooks/useNotification';
+import { NotificationTypes } from '../utils/consts';
 
 const GalleryPage = () => {
   const [isAlbumModalActive, setIsAlbumModalActive] = useState(false);
+  const { showNotification } = useNotification();
   const { user } = useUser();
   return (
     <div className='gallery-page'>
       <div className="gallery-page__container">
-        <div className="gallery-page__albums">
-          {user.albums
-            &&
-            <>
-              {user.albums && user.albums.map((album) => {
-                return <AlbumItem key={album.id} {...album} />
-              })}
-            </>
-          }
-          <button onClick={() => setIsAlbumModalActive(oldValue => !oldValue)}>
-            Add album
-          </button>
-        </div>
+        {user.albums
+          &&
+          <>
+            {user.albums && user.albums.map((album) => {
+              if (!album) {
+                showNotification("Can't fetch album", NotificationTypes.ERROR);
+                return <div>Error</div>
+              }
+              return <AlbumItem key={album.id} {...album} />
+            })}
+          </>
+        }
+        <button onClick={() => setIsAlbumModalActive(oldValue => !oldValue)}>
+          Add album
+        </button>
       </div>
       <ModalTemplate visible={isAlbumModalActive} setVisible={setIsAlbumModalActive} >
         <AlbumFormModal />
