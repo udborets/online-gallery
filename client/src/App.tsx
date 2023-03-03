96cbc7b854fbca840256eee3d38ace527f683447
Yury Borets <udborets@gmail.com>
2023-03-03 10:28:27 +0300

    add notification to root app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,24 @@ import { useEffect } from 'react';
 import NavBar from './components/NavBar'
 import useLogin from './hooks/useLogin';
 import PageRouter from './PageRouter';
+import Notification from './components/UI/Notification';
+import { NotificationTypes } from './utils/consts';
+import useNotification from './hooks/useNotification';
 
 export default function App() {
   const { checkAndLogin } = useLogin();
+  const { notification, setNotificationIsActive, } = useNotification();
   useEffect(() => {
     checkAndLogin();
   }, [])
   return (
     <>
+      <Notification
+        message={notification.message}
+        type={notification.type}
+        setIsActive={setNotificationIsActive}
+        isActive={notification.isActive}
+      />
       <NavBar />
       <PageRouter />
     </>
