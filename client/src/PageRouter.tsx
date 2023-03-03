132d7913ed6afeb64e1ff95beb5d79a4262682e8
Yury Borets <udborets@gmail.com>
2023-03-03 16:16:22 +0300

    add notfound page redirect in router

--- client/src/PageRouter.tsx
+++ client/src/PageRouter.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import useUser from './hooks/useUser'
 import { publicRoutes, authRoutes } from './utils/pageRoutes'
+import NotFoundPage from './pages/NotFoundPage';
 
 const PageRouter = () => {
   const { user } = useUser();
   return (
     <Routes>
+      <Route path={"*"} element={<NotFoundPage />} />
       {publicRoutes.map(({ path, Component }) => (
         <Route key={Date.now() * Math.random()} path={path} element={<Component />} />
       ))
