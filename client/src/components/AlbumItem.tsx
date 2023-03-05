0fc8da9b74bc14e26f823dd71efaad2c294973f5
Yury Borets <udborets@gmail.com>
2023-03-05 20:18:04 +0300

    delete old user album model usage

--- client/src/components/AlbumItem.tsx
+++ client/src/components/AlbumItem.tsx
@@ -1,55 +1,28 @@
 import { useQuery } from 'react-query/react';
 import { useNavigate } from 'react-router-dom';
+import useFirebase from '../hooks/useFirebase';
 
 import useNotification from "../hooks/useNotification";
-import useServer from "../hooks/useServer";
 import useUser from "../hooks/useUser";
-import { IdbAlbum } from "../models/dbTypes";
 import "../styles/components/AlbumItem.scss";
 import { NotificationTypes, RoutePaths } from "../utils/consts";
 
-const AlbumItem = (album: IdbAlbum) => {
+const AlbumItem = (albumId: string) => {
   const { user } = useUser();
   const navigate = useNavigate();
-  const { getAllPhotosByAlbumId } = useServer();
   const { showNotification } = useNotification();
-  if (!album) {
+  const { getAlbumFolderRef } = useFirebase()
+  if (!albumId) {
     showNotification(`error happened while fetching album`, NotificationTypes.ERROR);
     return <div>Error</div>;
   }
-  const { data: albumCoverFile } = useQuery({
-    queryFn: async () => {
-      const albumPhotos = await getAllPhotosByAlbumId(album.id);
-      if (!albumPhotos) {
-        showNotification("Can't get album photos", NotificationTypes.ERROR);
-        return null;
-      }
-      if (albumPhotos[0]) {
-        return albumPhotos[0].file;
-      }
-      return null;
-    },
-    onError: () => {
-      showNotification("Can't load image", NotificationTypes.ERROR)
-    },
-    queryKey: [`album${album?.id}`],
-    refetchInterval: 99999,
-    enabled: true,
-  })
+
   return (
-    <div className="album"
-      onClick={() => navigate(RoutePaths.USERS + "/" + user.id + RoutePaths.GALLERY + `/${album.id}`)}>
+    <div className="album">
       <div className="album__container">
-        {
-          albumCoverFile &&
-          <img
-            className="album__cover"
-            src={import.meta.env.VITE_REACT_APP_API_URL + "/" + albumCoverFile}
-            alt="cant load image"
-          />
-        }
+
         <span>
-          {album.name}
+          {albumId}
         </span>
 
       </div>
