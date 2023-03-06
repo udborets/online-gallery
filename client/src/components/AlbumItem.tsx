fc40a63c192396520f9f415c1d5b95ae47c80cae
Yury Borets <udborets@gmail.com>
2023-03-06 17:59:22 +0300

    started to fix album item

--- client/src/components/AlbumItem.tsx
+++ client/src/components/AlbumItem.tsx
@@ -1,30 +1,23 @@
-import { useQuery } from 'react-query/react';
-import { useNavigate } from 'react-router-dom';
-import useFirebase from '../hooks/useFirebase';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import useNotification from "../hooks/useNotification";
-import useUser from "../hooks/useUser";
 import "../styles/components/AlbumItem.scss";
 import { NotificationTypes, RoutePaths } from "../utils/consts";
 
 const AlbumItem = (albumId: string) => {
-  const { user } = useUser();
   const navigate = useNavigate();
   const { showNotification } = useNotification();
-  const { getAlbumFolderRef } = useFirebase()
-  if (!albumId) {
+  const { user_email } = useParams()
+  if (!albumId || !user_email) {
     showNotification(`error happened while fetching album`, NotificationTypes.ERROR);
-    return <div>Error</div>;
+    return <div>Error</div>
   }
-
   return (
     <div className="album">
       <div className="album__container">
-
-        <span>
+        <span onClick={() => navigate(RoutePaths.USERS + `/${user_email}` + RoutePaths.GALLERY + `/${albumId}`)}>
           {albumId}
         </span>
-
       </div>
     </div>
   )
