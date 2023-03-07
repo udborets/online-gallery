4fc1dfe5f03090269c05faaa15d65ba2c102f503
Yury Borets <udborets@gmail.com>
2023-03-07 22:46:39 +0300

    delete unused import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from 'react-redux';
