a356ac2efa468477763530e43ff95c6c0ac9c3fa
Yury Borets <udborets@gmail.com>
2023-03-09 17:42:56 +0300

    fix gallery content displaying logic

--- client/src/pages/GalleryPage.tsx
+++ client/src/pages/GalleryPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from 'react-query/react';
 
 import AlbumFormModal from '../components/modals/AlbumFormModal';
@@ -7,16 +7,30 @@ import ModalTemplate from '../components/modals/templates/ModalTemplate';
 import useUser from '../hooks/useUser';
 import "../styles/pages/GalleryPage.scss";
 import useFirebase from '../hooks/useFirebase';
-import { RoutePaths } from '../utils/consts';
+import { NotificationTypes, RoutePaths } from '../utils/consts';
+import { getUserByName } from '../query';
+import useNotification from './../hooks/useNotification';
 
 const GalleryPage = () => {
   const [isAlbumModalActive, setIsAlbumModalActive] = useState(false);
   const { user } = useUser();
-  const { getRefItems, getRef } = useFirebase();
+  const { getRefItems } = useFirebase();
   const navigate = useNavigate();
+  const { user_name } = useParams();
+  const { showNotification } = useNotification();
+  const isOwnGallery = user_name === user.name;
+  if (!user_name) {
+    showNotification("Error while trying to get user name", NotificationTypes.ERROR);
+    return <div>Error!</div>
+  }
   const albums = useQuery({
     queryFn: async () => {
-      const fetchedUserAlbums = (await getRefItems(getRef(user.email))).prefixes;
+      const fetchedUser = await getUserByName(user_name);
+      if (!fetchedUser) {
+        showNotification("Error happened while trying to fetch user", NotificationTypes.ERROR);
+        return null;
+      }
+      const fetchedUserAlbums = (await getRefItems(fetchedUser.email)).prefixes;
       return fetchedUserAlbums;
     },
   });
@@ -38,13 +52,19 @@ const GalleryPage = () => {
           </div>)
         )
         }
-        <button onClick={() => setIsAlbumModalActive(oldValue => !oldValue)}>
-          Add album
-        </button>
+        {
+          isOwnGallery &&
+          <button onClick={() => setIsAlbumModalActive(oldValue => !oldValue)}>
+            Add album
+          </button>
+        }
       </div>
-      <ModalTemplate visible={isAlbumModalActive} setVisible={setIsAlbumModalActive} >
-        <AlbumFormModal />
-      </ModalTemplate>
+      {
+        isOwnGallery &&
+        <ModalTemplate visible={isAlbumModalActive} setVisible={setIsAlbumModalActive} >
+          <AlbumFormModal />
+        </ModalTemplate>
+      }
     </div>
   )
 }
