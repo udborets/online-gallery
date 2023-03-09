960af10a4031c721edea169ff53b5cfa93b6469d
Yury Borets <udborets@gmail.com>
2023-03-09 19:10:52 +0300

    replace 'me' page to 'user' page

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { useNavigate, useParams } from 'react-router-dom';
+import useNotification from '../hooks/useNotification';
+import useUser from '../hooks/useUser';
 
-import { getUserByName } from "../query";
-import { RoutePaths } from '../utils/consts';
+import { getUserByName, getUsers, updateUserName } from "../query";
+import { NotificationTypes, RoutePaths } from '../utils/consts';
 
 const UserByIdPage = () => {
   const navigate = useNavigate();
   const { user_name } = useParams();
+  const { user, setName } = useUser();
+  const [newName, setNewName] = useState('');
+  const { showNotificationWithTimeout, showNotification } = useNotification();
   if (!user_name) {
     navigate(RoutePaths.NOTFOUND);
     return <></>;
@@ -32,7 +38,41 @@ const UserByIdPage = () => {
   }
   return (
     <div className="users-page">
-      {fetchedUser ? fetchedUser.name : 'error'}
+      <button
+        onClick={async () => console.log(await getUserByName(user.name))}
+      >
+        show user info
+      </button>
+      <button onClick={async () => {
+        const allUserNames = (await getUsers()).map((fetchedUser) => fetchedUser.name);
+        if (allUserNames.find((fetchedUserName) => fetchedUserName === newName)?.length) {
+          showNotificationWithTimeout(
+            "User with this user name already exists, please, choose another one",
+            NotificationTypes.WARNING,
+            5000,
+          );
+          return;
+        }
+        const userWithUpdatedName = await updateUserName(user.name, newName);
+        if (userWithUpdatedName.name === newName) {
+          showNotificationWithTimeout(
+            "Successfully updated name",
+            NotificationTypes.SUCCESS,
+            5000
+          );
+          setName(newName);
+          setNewName("");
+          return;
+        }
+        showNotification(
+          "Error happened while trying to update user name",
+          NotificationTypes.ERROR,
+        );
+      }}
+      >
+        Set name
+      </button>
+      <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
     </div>
   )
 }
