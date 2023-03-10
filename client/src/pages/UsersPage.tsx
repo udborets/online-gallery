6b67f056bad06fa3e8cbaa0cf001d4cb8182aa73
Yury Borets <udborets@gmail.com>
2023-03-10 15:53:13 +0300

    add list of users in users page

--- client/src/pages/UsersPage.tsx
+++ client/src/pages/UsersPage.tsx
@@ -1,8 +1,23 @@
 import "../styles/pages/UsersPage.scss";
+import { useQuery } from 'react-query';
+import { getUsers } from "../query";
+import { useNavigate } from "react-router-dom";
+import { RoutePaths } from "../utils/consts";
 
 const UsersPage = () => {
+  const { data: users } = useQuery({
+    queryFn: async () => {
+      return await getUsers();
+    }
+  });
+  const navigate = useNavigate();
   return (
     <div className="users-page">
+      {
+        users?.map((user) => {
+          return <div key={user.id} onClick={() => navigate(RoutePaths.USERS + '/' + user.id + '/gallery')}>{user.id}</div>
+        })
+      }
     </div>
   )
 }
