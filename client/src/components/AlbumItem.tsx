14f198dd245f1799188fd0f4c80280d5a30496a7
Yury Borets <udborets@gmail.com>
2023-03-11 18:31:56 +0300

    fix album item

--- client/src/components/AlbumItem.tsx
+++ client/src/components/AlbumItem.tsx
@@ -1,22 +1,57 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from 'react-query/react';
 
 import useNotification from "../hooks/useNotification";
 import "../styles/components/AlbumItem.scss";
 import { NotificationTypes, RoutePaths } from "../utils/consts";
+import useFirebase from './../hooks/useFirebase';
 
-const AlbumItem = (albumId: string) => {
+const AlbumItem = ({ albumName }: { albumName: string }) => {
   const navigate = useNavigate();
   const { showNotification } = useNotification();
   const { user_id } = useParams();
-  if (!albumId || !user_id) {
+  const { getRefUrls } = useFirebase();
+  const cover = useQuery({
+    queryFn: async () => {
+      return (await getRefUrls(user_id + '/' + albumName))[0] ?? null;
+    },
+    queryKey: [albumName],
+    refetchInterval: 100000000,
+    refetchOnWindowFocus: false,
+  });
+  if (!albumName || !user_id) {
     showNotification(`error happened while fetching album`, NotificationTypes.ERROR);
     return <div>Error</div>
+  }
+  if (cover.isLoading) {
+    return <div className='album'>
+      Loading...
+    </div>
   }
   return (
     <div className="album">
       <div className="album__container">
-        <span onClick={() => navigate(RoutePaths.USERS + `/${user_id}` + RoutePaths.GALLERY + `/${albumId}`)}>
-          {albumId}
+        <div
+          className="album__cover"
+          onClick={() => navigate(RoutePaths.USERS
+            + `/${user_id}`
+            + RoutePaths.GALLERY
+            + `/${albumName}`)}
+        >
+          {
+            cover &&
+              cover.data ?
+              <img
+                src={cover.data}
+                alt="can't load image :("
+                className='album__image'
+              />
+              :
+              albumName
+          }
+        </div>
+        <span>
+          {albumName}
         </span>
       </div>
     </div>
